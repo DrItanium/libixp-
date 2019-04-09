f53d6faac73b53763ec25b35e4b1f35a4144a8e8
Josh Scoggins <jscoggins@parasoft.com>
2019-04-09 16:09:38 -0700

    purge the old mk and util directories

--- mk/ixp.mk
+++
@@ -1,8 +0,0 @@
-VERSION = 0.5
-
-COPYRIGHT = ©2010 Kris Maglione
-
-$(ROOT)/include/ixp.h: $(ROOT)/config.mk
-CFLAGS += '-DVERSION=\"$(VERSION)\"' '-DCOPYRIGHT=\"$(COPYRIGHT)\"'
-CXXFLAGS += '-DVERSION=\"$(VERSION)\"' '-DCOPYRIGHT=\"$(COPYRIGHT)\"'
-
