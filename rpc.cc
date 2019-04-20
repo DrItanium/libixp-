6752e47eea5e9d2ed2bdafcdceef3240f2b37291
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-04-19 22:50:58 -0700

    Fixed rpc.cc

--- rpc.cc
+++ rpc.cc
@@ -11,56 +11,56 @@
 namespace ixp {
 namespace {
 void
-initrpc(IxpClient *mux, IxpRpc *r)
+initrpc(Client *mux, Rpc *r)
 {
 	r->mux = mux;
 	r->waiting = 1;
 	r->r.mutex = (decltype(r->r.mutex))&mux->lk;
 	r->p = nullptr;
-	thread->initrendez(&r->r);
+	concurrency::threadModel->initrendez(&r->r);
 }
 
 void
-freemuxrpc(IxpRpc *r)
+freemuxrpc(Rpc *r)
 {
-	thread->rdestroy(&r->r);
+	concurrency::threadModel->rdestroy(&r->r);
 }
 
-IxpFcall*
-muxrecv(IxpClient *mux)
+Fcall*
+muxrecv(Client *mux)
 {
-	IxpFcall *f = nullptr;
-	thread->lock(&mux->rlock);
-	if(ixp_recvmsg(mux->fd, &mux->rmsg) == 0)
+	Fcall *f = nullptr;
+	concurrency::threadModel->lock(&mux->rlock);
+	if(recvmsg(mux->fd, &mux->rmsg) == 0)
 		goto fail;
 	f = (decltype(f))ixp::emallocz(sizeof *f);
-	if(ixp_msg2fcall(&mux->rmsg, f) == 0) {
+	if(msg2fcall(&mux->rmsg, f) == 0) {
 		free(f);
 		f = nullptr;
 	}
 fail:
-	thread->unlock(&mux->rlock);
+	concurrency::threadModel->unlock(&mux->rlock);
 	return f;
 }
 
 
 void
-electmuxer(IxpClient *mux)
+electmuxer(Client *mux)
 {
-	IxpRpc *rpc;
+	Rpc *rpc;
 
 	/* if there is anyone else sleeping, wake them to mux */
 	for(rpc=mux->sleep.next; rpc != &mux->sleep; rpc = rpc->next){
 		if(!rpc->async){
 			mux->muxer = rpc;
-			thread->wake(&rpc->r);
+			concurrency::threadModel->wake(&rpc->r);
 			return;
 		}
 	}
 	mux->muxer = nullptr;
 }
 void
-enqueue(IxpClient *mux, IxpRpc *r)
+enqueue(Client *mux, Rpc *r)
 {
 	r->next = mux->sleep.next;
 	r->prev = &mux->sleep;
@@ -69,7 +69,7 @@ enqueue(IxpClient *mux, IxpRpc *r)
 }
 
 void
-dequeue(IxpClient *, IxpRpc *r)
+dequeue(Client *, Rpc *r)
 {
 	r->next->prev = r->prev;
 	r->prev->next = r->next;
@@ -78,10 +78,10 @@ dequeue(IxpClient *, IxpRpc *r)
 }
 
 int 
-gettag(IxpClient *mux, IxpRpc *r)
+gettag(Client *mux, Rpc *r)
 {
 	int i, mw;
-	IxpRpc **w;
+	Rpc **w;
 
 	for(;;){
 		/* wait for a free tag */
@@ -101,7 +101,7 @@ gettag(IxpClient *mux, IxpRpc *r)
 				mux->mwait = mw;
 				break;
 			}
-			thread->sleep(&mux->tagrend);
+			concurrency::threadModel->sleep(&mux->tagrend);
 		}
 
 		i=mux->freetag;
@@ -125,48 +125,48 @@ Found:
 }
 
 void
-puttag(IxpClient *mux, IxpRpc *r)
+puttag(Client *mux, Rpc *r)
 {
 	auto i = r->tag - mux->mintag;
 	assert(mux->wait[i] == r);
 	mux->wait[i] = nullptr;
 	mux->nwait--;
 	mux->freetag = i;
-	thread->wake(&mux->tagrend);
+	concurrency::threadModel->wake(&mux->tagrend);
 	freemuxrpc(r);
 }
 int
-sendrpc(IxpRpc *r, IxpFcall *f)
+sendrpc(Rpc *r, Fcall *f)
 {
 	auto ret = 0;
 	auto mux = r->mux;
 	/* assign the tag, add selves to response queue */
-	thread->lock(&mux->lk);
+	concurrency::threadModel->lock(&mux->lk);
 	r->tag = gettag(mux, r);
 	f->hdr.tag = r->tag;
 	enqueue(mux, r);
-	thread->unlock(&mux->lk);
+	concurrency::threadModel->unlock(&mux->lk);
 
-	thread->lock(&mux->wlock);
-	if(!ixp_fcall2msg(&mux->wmsg, f) || !ixp_sendmsg(mux->fd, &mux->wmsg)) {
-		/* ixp_werrstr("settag/send tag %d: %r", tag); fprint(2, "%r\n"); */
-		thread->lock(&mux->lk);
+	concurrency::threadModel->lock(&mux->wlock);
+	if(!fcall2msg(&mux->wmsg, f) || !sendmsg(mux->fd, &mux->wmsg)) {
+		/* werrstr("settag/send tag %d: %r", tag); fprint(2, "%r\n"); */
+		concurrency::threadModel->lock(&mux->lk);
 		dequeue(mux, r);
 		puttag(mux, r);
-		thread->unlock(&mux->lk);
+		concurrency::threadModel->unlock(&mux->lk);
 		ret = -1;
 	}
-	thread->unlock(&mux->wlock);
+	concurrency::threadModel->unlock(&mux->wlock);
 	return ret;
 }
 void
-dispatchandqlock(IxpClient *mux, IxpFcall *f)
+dispatchandqlock(Client *mux, Fcall *f)
 {
 	int tag;
-	IxpRpc *r2;
+	Rpc *r2;
 
 	tag = f->hdr.tag - mux->mintag;
-	thread->lock(&mux->lk);
+	concurrency::threadModel->lock(&mux->lk);
 	/* hand packet to correct sleeper */
 	if(tag < 0 || tag >= mux->mwait) {
 		fprintf(stderr, "libixp: received unfeasible tag: %d (min: %d, max: %d)\n", f->hdr.tag, mux->mintag, mux->mintag+mux->mwait);
@@ -179,62 +179,62 @@ dispatchandqlock(IxpClient *mux, IxpFcall *f)
 	}
 	r2->p = f;
 	dequeue(mux, r2);
-	thread->wake(&r2->r);
+	concurrency::threadModel->wake(&r2->r);
 	return;
 fail:
-	ixp_freefcall(f);
+	freefcall(f);
 	free(f);
 }
 } // end namespace
 
 void
-muxinit(IxpClient *mux)
+muxinit(Client *mux)
 {
 	mux->tagrend.mutex = &mux->lk;
 	mux->sleep.next = &mux->sleep;
 	mux->sleep.prev = &mux->sleep;
-	thread->initmutex(&mux->lk);
-	thread->initmutex(&mux->rlock);
-	thread->initmutex(&mux->wlock);
-	thread->initrendez(&mux->tagrend);
+	concurrency::threadModel->initmutex(&mux->lk);
+	concurrency::threadModel->initmutex(&mux->rlock);
+	concurrency::threadModel->initmutex(&mux->wlock);
+	concurrency::threadModel->initrendez(&mux->tagrend);
 }
 
 void
-muxfree(IxpClient *mux)
+muxfree(Client *mux)
 {
-	thread->mdestroy(&mux->lk);
-	thread->mdestroy(&mux->rlock);
-	thread->mdestroy(&mux->wlock);
-	thread->rdestroy(&mux->tagrend);
+	concurrency::threadModel->mdestroy(&mux->lk);
+	concurrency::threadModel->mdestroy(&mux->rlock);
+	concurrency::threadModel->mdestroy(&mux->wlock);
+	concurrency::threadModel->rdestroy(&mux->tagrend);
 	free(mux->wait);
 }
 
 
-IxpFcall*
-muxrpc(IxpClient *mux, IxpFcall *tx)
+Fcall*
+muxrpc(Client *mux, Fcall *tx)
 {
-	IxpRpc r;
-	IxpFcall *p;
+	Rpc r;
+	Fcall *p;
 
 	initrpc(mux, &r);
 	if(sendrpc(&r, tx) < 0)
 		return nullptr;
 
-	thread->lock(&mux->lk);
+	concurrency::threadModel->lock(&mux->lk);
 	/* wait for our packet */
 	while(mux->muxer && mux->muxer != &r && !r.p)
-		thread->sleep(&r.r);
+		concurrency::threadModel->sleep(&r.r);
 
 	/* if not done, there's no muxer; start muxing */
 	if(!r.p){
 		assert(mux->muxer == nullptr || mux->muxer == &r);
 		mux->muxer = &r;
 		while(!r.p){
-			thread->unlock(&mux->lk);
+			concurrency::threadModel->unlock(&mux->lk);
 			p = muxrecv(mux);
             if (!p) {
 				/* eof -- just give up and pass the buck */
-				thread->lock(&mux->lk);
+				concurrency::threadModel->lock(&mux->lk);
 				dequeue(mux, &r);
 				break;
 			}
@@ -244,9 +244,9 @@ muxrpc(IxpClient *mux, IxpFcall *tx)
 	}
 	p = r.p;
 	puttag(mux, &r);
-	thread->unlock(&mux->lk);
+	concurrency::threadModel->unlock(&mux->lk);
     if (!p)
-		ixp_werrstr("unexpected eof");
+		werrstr("unexpected eof");
 	return p;
 }
 } // end namespace ixp
