bfaa7bda072cbe959b45a07b25d77b26189779f1
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-04-21 13:12:37 -0700

    Use packUnpackMany
    Added support for passing values by reference

--- convert.cc
+++ convert.cc
@@ -267,9 +267,10 @@ Msg::pdata(char **data, uint len) {
  */
 void
 Qid::packUnpack(Msg& msg) {
-	msg.pu8(&type);
-	msg.pu32(&version);
-	msg.pu64(&path);
+    msg.packUnpackMany(type, version, path);
+	//msg.pu8(&type);
+	//msg.pu32(&version);
+	//msg.pu64(&path);
 }
 
 void
