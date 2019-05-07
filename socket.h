9ec5bd2091f4105fd7ba0c8d5e564ce565231f81
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-05-06 20:46:47 -0700

    Define the Connection structure

--- socket.h
+++ socket.h
@@ -13,5 +13,32 @@ namespace jyq {
     int announce(const std::string&);
     uint sendmsg(int, Msg*);
     uint recvmsg(int, Msg*);
+    /**
+     * Generic connection to a file of some kind!
+     */
+    class Connection {
+        public:
+            static Connection dial(const std::string&);
+            static Connection announce(const std::string&);
+        public:
+            explicit Connection(int fid);
+            ~Connection() = default;
+            int getFid() const noexcept { return _fid; }
+            ssize_t write(const std::string& msg);
+            ssize_t write(char* c, size_t count);
+            ssize_t write(const std::string& msg, size_t count);
+            ssize_t read(std::string& msg, size_t count);
+            ssize_t read(char* c, size_t count);
+            uint sendmsg(Msg& msg);
+            uint recvmsg(Msg& msg);
+            bool shutdown(int how);
+            bool close();
+            operator int() const;
+        private:
+            int mread(Msg& msg, size_t count);
+            int readn(Msg& msg, size_t count);
+        private:
+            int _fid;
+    };
 } // end namespace jyq
 #endif // end LIBJYQ_SOCKET_H__
