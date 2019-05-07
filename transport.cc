5f1e0e13c5749c5c8a87301c6a4a124c224443d7
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-05-06 20:48:51 -0700

    Import the connection object

--- transport.cc
+++ transport.cc
@@ -12,6 +12,7 @@
 #include <unistd.h>
 #include "Msg.h"
 #include "jyq.h"
+#include "socket.h"
 
 namespace jyq {
 namespace {
@@ -114,6 +115,88 @@ recvmsg(int fd, Msg *msg) {
 
 	msg->end = msg->pos;
 	return msize;
+}
+int
+Connection::readn(Msg& msg, size_t count) {
+    auto num = count;
+    while (num > 0) {
+        auto r = mread(msg, num);
+        if (r == -1 && errno == EINTR) {
+            continue;
+        }
+        if (r == 0) {
+            werrstr("broken pipe: %s", errbuf());
+            return count = num;
+        }
+        num -= r;
+    }
+    return count - num;
+}
+
+int
+Connection::mread(Msg& msg, size_t count) {
+    //std::cout << "fd = " << fd << std::endl;
+	auto n = msg.end - msg.pos;
+	if (n <= 0) {
+		werrstr("buffer full");
+		return -1;
+	}
+	if(n > count) {
+		n = count;
+    }
+
+    auto r = read(msg.pos, n);
+	if(r > 0) {
+        msg.pos += r;
+    }
+	return r;
+}
+
+uint
+Connection::sendmsg(Msg& msg) {
+    msg.pos = msg.data;
+	while(msg.pos < msg.end) {
+        if (auto r = write(msg.pos, msg.end - msg.pos); r < 1) {
+			if(errno == EINTR) {
+				continue;
+            }
+			werrstr("broken pipe: %s", errbuf());
+			return 0;
+		} else {
+            msg.pos += r;
+        }
+	}
+    return msg.pos - msg.data;
+}
+
+uint
+Connection::recvmsg(Msg& msg) {
+    static constexpr auto SSize = 4;
+	uint32_t msize;
+
+    msg.setMode(Msg::Mode::Unpack);
+	msg.pos = msg.data;
+	msg.end = msg.data + msg.size();
+    if (readn(msg, SSize) != SSize) {
+        return 0;
+    }
+
+	msg.pos = msg.data;
+    msg.pu32(&msize);
+
+	uint32_t size = msize - SSize;
+	if(size >= msg.end - msg.pos) {
+		werrstr("message too large");
+		return 0;
+	}
+    if (readn(msg, size) != size) {
+		werrstr("message incomplete");
+		return 0;
+	}
+
+    msg.end = msg.pos;
+	return msize;
+
 }
 
 } // end namespace jyq
