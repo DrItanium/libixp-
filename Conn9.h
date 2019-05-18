786244e9b065924cfdb16eef31a3c4c99b5dd18a
Josh Scoggins <jscoggins@parasoft.com>
2019-05-17 17:15:10 -0700

    Rearrange code in Conn9.h

--- Conn9.h
+++ Conn9.h
@@ -23,62 +23,64 @@
 namespace jyq {
 struct Srv9;
 struct Conn9 {
-    using TagMap = jyq::Map<uint16_t, Req9>;
-    private:
-    TagMap    _tagmap;
-    Fid::Map  _fidmap;
-    Srv9*   _srv;
-	Conn*	_conn;
-	Mutex	_rlock;
-	Mutex	_wlock;
-    Msg		_rmsg;
-    Msg		_wmsg;
     public:
-    Fid::Map& getFidMap() noexcept { return _fidmap; }
-    TagMap& getTagMap() noexcept { return _tagmap; }
-    auto getSrv() noexcept { return _srv; }
-    void setSrv(Srv9* value) { _srv = value; }
-    auto getConn() noexcept { return _conn; }
-    void setConn(Conn* value) noexcept { _conn = value; }
-    Msg& getRMsg() noexcept { return _rmsg; }
-    Msg& getWMsg() noexcept { return _wmsg; }
-    Mutex& getRLock() noexcept { return _rlock; }
-    Mutex& getWLock() noexcept { return _wlock; }
-    void alloc(uint n);
-    constexpr auto getReferenceCount() const noexcept { return _ref; }
-    constexpr auto referenceCountGreaterThan(int count) const noexcept { return _ref > count; }
-    constexpr auto referenceCountIs(int count) const noexcept { return _ref == count; }
-    void incrementReferenceCount() noexcept { ++_ref; }
-    void decrementReferenceCount() noexcept { --_ref; }
-    Req9* retrieveTag(uint16_t id);
-    Fid* retrieveFid(int id);
-    bool removeTag(uint16_t id);
-    bool removeFid(int id);
-    template<typename T>
-    void tagExec(std::function<void(T, TagMap::iterator)> op, T context) {
-        _tagmap.exec<T>(op, context);
-    }
-    template<typename T>
-    void fidExec(std::function<void(T, Fid::Map::iterator)> op, T context) {
-        _fidmap.exec<T>(op, context);
-    }
+        using TagMap = jyq::Map<uint16_t, Req9>;
+    public:
+        Conn9() = default;
+        ~Conn9() = default;
+        Fid::Map& getFidMap() noexcept { return _fidmap; }
+        TagMap& getTagMap() noexcept { return _tagmap; }
+        auto getSrv() noexcept { return _srv; }
+        void setSrv(Srv9* value) { _srv = value; }
+        auto getConn() noexcept { return _conn; }
+        void setConn(Conn* value) noexcept { _conn = value; }
+        Msg& getRMsg() noexcept { return _rmsg; }
+        Msg& getWMsg() noexcept { return _wmsg; }
+        Mutex& getRLock() noexcept { return _rlock; }
+        Mutex& getWLock() noexcept { return _wlock; }
+        void alloc(uint n);
+        constexpr auto getReferenceCount() const noexcept { return _ref; }
+        constexpr auto referenceCountGreaterThan(int count) const noexcept { return _ref > count; }
+        constexpr auto referenceCountIs(int count) const noexcept { return _ref == count; }
+        void incrementReferenceCount() noexcept { ++_ref; }
+        void decrementReferenceCount() noexcept { --_ref; }
+        Req9* retrieveTag(uint16_t id);
+        Fid* retrieveFid(int id);
+        bool removeTag(uint16_t id);
+        bool removeFid(int id);
+        template<typename T>
+            void tagExec(std::function<void(T, TagMap::iterator)> op, T context) {
+                _tagmap.exec<T>(op, context);
+            }
+        template<typename T>
+            void fidExec(std::function<void(T, Fid::Map::iterator)> op, T context) {
+                _fidmap.exec<T>(op, context);
+            }
 
-    /**
-     * Increment the reference count
-     */
-    Conn9& operator++() {
-        incrementReferenceCount();
-        return *this;
-    }
-    /**
-     * decrement the reference count
-     */
-    Conn9& operator--() {
-        decrementReferenceCount();
-        return *this;
-    }
+        /**
+         * Increment the reference count
+         */
+        Conn9& operator++() {
+            incrementReferenceCount();
+            return *this;
+        }
+        /**
+         * decrement the reference count
+         */
+        Conn9& operator--() {
+            decrementReferenceCount();
+            return *this;
+        }
 
     private:
+        TagMap    _tagmap;
+        Fid::Map  _fidmap;
+        Srv9*   _srv;
+        Conn*	_conn;
+        Mutex	_rlock;
+        Mutex	_wlock;
+        Msg		_rmsg;
+        Msg		_wmsg;
         int _ref;
 };
 } // end namespace jyq
